8eaaa3488d7c6ce3d0fad5c2844ea093ca8a42c4
Tayyaba260 <tayyabatariq957@gmail.com>
2024-04-05 03:22:05 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+//printing a welcome message
+console.log("\n\tWelcome To \`CodewithTayyaba26\` -CLI Simple Calculator");
+
+//asking question to users from inquirer
+
+let answers = await inquirer.prompt([
+    {message:"Enter your first number" , type:"number", name: "firstNumber"},
+    {message:"Enter second number" , type:"number", name: "secondNumber"},
+    {
+        message:"select any operator to perform operations",
+        type:"list",
+        name:"Operator",
+        choices: ["Addition","Subtraction","Multiplication","Division"],
+    },
+]);
+
+//conditional statement if/else
+if(answers.Operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+
+else if(answers.Operator === "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+
+else if(answers.Operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+
+else if(answers.Operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+
+else{
+    console.log("invalid input")
+}
